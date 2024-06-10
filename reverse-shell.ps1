7383d58800bc0f0b3f5f85714da33910c3eb46da
mephisto76 <solzer@gmx.de>
2024-06-10 08:43:35 +0200

    Update reverse-shell.ps1

--- reverse-shell.ps1
+++ reverse-shell.ps1
@@ -1,1 +1,9 @@
-dies ist ein test
+# $ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/mephisto76/coding/reverse-shell.ps1
+# Invoke-Expression $($ScriptFromGithHub.Content)
+
+
+Write-Host " "
+Write-Host "Here are the top 10 CPU consuming processes right now"
+Write-Host " "
+Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
+
